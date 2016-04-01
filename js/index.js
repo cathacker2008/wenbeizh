3ce12d0adde3bcd9ab455231b8177f0a8b89730f
zhanghan <cathacker2008@163.com>
2016-04-01 13:54:03 +0800

    增加项目截图

--- js/index.js
+++ js/index.js
@@ -7,21 +7,19 @@ $(document).ready(function(){
 			$('.content').css('height',700);
 		}
 	}
-	var t;
-	var loopPic = function(){
-		var i=1;
-		$('.content').css('backgroundImage','url(./images/bg0.jpg)');
-		//var cIn = $('.c-in')[0];
-		//var cOut = $('.c-out')[0];
-		t=window.setInterval(function(){
-			$('.content').css('backgroundImage','url(./images/bg'+i+'.jpg)');
-			i++;	
-			if(i==6){
-				i=0;
-			}
-		},3000);
+	var pageIndex=0;imgArr = ['al.jpg','al2.jpg','deskManage.jpg','dt1.png','dt1.png','dt2.png','dt3.png','dt4.jpg','dt5.png','dt6.png','dt7.png','home.png','mm1.jpg','tp1.jpg','um.jpg'];
+	var len=imgArr.length;
+	var next = function(){
+		pageIndex = ++pageIndex >= len?0:pageIndex; 
+		$('.content').css('backgroundImage','url(./img/'+imgArr[pageIndex]);
 	}
+	var  pre= function(){
+		pageIndex = --pageIndex < 0 ?len-1:pageIndex; 
+		$('.content').css('backgroundImage','url(./img/'+imgArr[pageIndex]);
+	}
+	$('.left').bind('click',pre);
+	$('.right').bind('click',next);
+	$('.content').css('backgroundImage','url(./img/'+imgArr[0]);
 	resetSize();
-	loopPic();
 	$(window).resize(resetSize);
 });
